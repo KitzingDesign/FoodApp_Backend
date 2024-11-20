780b8006488af3eb2be02cdd16791d758e59f4c6
Jakob Kitzing <Jakob.kitzing@hotmail.com>
2024-11-20 16:13:17 +0100

    change in allowed domains

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,13 @@
 const allowedOrigins = [
   "https://www.matmatmaten.com",
-  "http://localhost:5173", //development origin
+  "https://www.api.matmatmaten.com",
   "https://cook-book-app-seven.vercel.app",
 ];
+
+// Conditionally include localhost for development
+if (process.env.NODE_ENV !== "production") {
+  allowedOrigins.push("http://localhost:5173");
+  allowedOrigins.push("http://192.168.0.185:5173"); // Development origin
+}
 
 module.exports = allowedOrigins;
